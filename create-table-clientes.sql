cd032c50cc743bb20f8d33c29e503481ebdeb16d
Erick <erick1junior@hotmail.com>
2022-02-22 03:17:06 -0300

    add files

---
+++ create-table-clientes.sql
@@ -0,0 +1,25 @@
+USE [SUCOS_VENDAS]
+GO
+CREATE TABLE TABELA_CLIENTES (
+	[CPF] [char](11) NOT NULL,
+	[NOME] [varchar](100) NULL,
+	[ENDEREÇO1] [varchar](150) NULL,
+	[ENDEREÇO2] [varchar](150) NULL,
+	[BAIRRO] [varchar](50) NULL,
+	[CIDADE] [varchar](50) NULL,
+	[ESTADO] [varchar](50) NULL,
+	[CEP] [char](8) NULL,
+	[DATA DE NASCIMENTO] [date] NULL,
+	[IDADE] [smallint] NULL,
+	[SEXO] [char](1) NULL,
+	[LIMITE DE CREDITO] [money] NULL,
+	[VOLUME DE COMPRA] [float] NULL,
+	[PRIMEIRA COMPRA] [bit] NULL,);
+
+ALTER TABLE [TABELA_CLIENTES]
+ADD CONSTRAINT PK_TABELA_CLIENTES
+PRIMARY KEY CLUSTERED ([CPF]);
+
+INSERT INTO [TABELA_CLIENTES] ([CPF], [NOME], [ENDEREÇO1], [ENDEREÇO2], [BAIRRO], [CIDADE], [ESTADO], [CEP], [DATA DE NASCIMENTO], [IDADE], [SEXO], [LIMITE DE CREDITO], [VOLUME DE COMPRA],[PRIMEIRA COMPRA]) VALUES ('06103718864', 'Rosangela Aparecida Silverio', 'Rua das Laranjeiras', 'Rua Luis Barbalho', 'Demarchi', 'São Bernardo do Campo', 'São Paulo', '09820030', '1965-01-26', '56', 'F', 50.00, 2.50, 1)
+INSERT INTO [TABELA_CLIENTES] ([CPF], [NOME], [ENDEREÇO1], [ENDEREÇO2], [BAIRRO], [CIDADE], [ESTADO], [CEP], [DATA DE NASCIMENTO], [IDADE], [SEXO], [LIMITE DE CREDITO], [VOLUME DE COMPRA],[PRIMEIRA COMPRA]) VALUES ('41295181860', 'Erick Silverio Blass', 'Rua das Flores', 'Rua Luis Silva', 'Lauro Gomes', 'São Caetano', 'São Paulo', '09820120', '2003-06-19', '18', 'M', 50.00, 2.50, 0);
+INSERT INTO [TABELA_CLIENTES] ([CPF], [NOME], [ENDEREÇO1], [ENDEREÇO2], [BAIRRO], [CIDADE], [ESTADO], [CEP], [DATA DE NASCIMENTO], [IDADE], [SEXO], [LIMITE DE CREDITO], [VOLUME DE COMPRA],[PRIMEIRA COMPRA]) VALUES ('12450587819', 'Vinicius Silverio Rocca', 'Rua das Seringueiras', 'Rua Luis Gomes', 'Assunção', 'Santo André', 'São Paulo', '09820060', '1994-07-29', '30', 'M', 50.00, 2.50, 1);
